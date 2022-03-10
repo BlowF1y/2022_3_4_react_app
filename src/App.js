10fd4511b82e1866649f966c6c45ed28a06d090f
BlowF1y <blowfly0258@gmail.com>
2022-03-10 16:48:51 +0900

    faker add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import logo from './LGM_ingame_2.jpg';
 import './App.css';
+import { faker } from "@faker-js/faker";
 
 function App() {
+  const h1Element = <h1>제목 태그 입니다</h1>
+  const imgElement = <img src={logo} className="App-logo" alt="logo" />
+
+        testData.map(function(contents){
+          return <li>{contents}</li>
+        })
+
+        testData.map((contents)=><li>{contents}</li>)
   return (
+   
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      { h1Element }
+      { imgElement }
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+
+        </ul>
       </header>
     </div>
   );
